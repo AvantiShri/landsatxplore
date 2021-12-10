c1169cb72b207aff948122510bf0809e6cb5b942
Av Shrikumar <avanti.shrikumar@gmail.com>
2021-12-10 12:21:02 -0800

    Update earthexplorer.py

--- landsatxplore/earthexplorer.py
+++ landsatxplore/earthexplorer.py
@@ -25,7 +25,7 @@ DATA_PRODUCTS = {
     "landsat_8_c1": "5e83d0b84df8d8c2",
     "landsat_tm_c2_l1": "5e83d0a0f94d7d8d",
     "landsat_etm_c2_l1": "5e83d0d0d2aaa488",
-    "landsat_ot_c2_l1": "5e81f14f59432a27",
+    "landsat_ot_c2_l1": "5e81f14ff4f9941c",
     "landsat_tm_c2_l2": "5e83d11933473426",
     "landsat_etm_c2_l2": "5e83d12aada2e3c5",
     "landsat_ot_c2_l2": "5e83d14f30ea90a9",
